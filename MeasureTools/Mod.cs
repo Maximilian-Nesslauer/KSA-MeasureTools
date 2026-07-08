8f2f8af143196df6ab104da484a18535349d9c87
Maximilian Neßlauer <dev@max-nesslauer.com>
2026-07-08 13:42:17 +0200

    Bump tested game version to v2026.7.4.4860

--- MeasureTools/Mod.cs
+++ MeasureTools/Mod.cs
@@ -14,7 +14,7 @@ public sealed class Mod
 {
     private static Harmony? _harmony;
 
-    private const string TestedGameVersion = "v2026.7.3.4826";
+    private const string TestedGameVersion = "v2026.7.4.4860";
 
     [StarMapAllModsLoaded]
     public void OnFullyLoaded()
